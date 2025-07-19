0414c9fa64a6a0009628077b249274040ef81ee2
Abdurahmon <abdurahmon1112qwe@gmail.com>
2025-07-19 10:36:18 +0500

    Blog

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Button, Input, Modal } from 'antd'
 import { Header } from './Components/Header'
 import CarouselComponent from './Components/Carousel'
+import Blog from './Components/Blog'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <>
      <Header/>
      <CarouselComponent/>
+     <Blog/>
     </>
   )
 }
