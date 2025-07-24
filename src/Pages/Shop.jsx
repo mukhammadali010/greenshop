c150c7fc708b341bce77aa56dd7cfd37a0e9f2e7
saidalo09 <ssaidakbarov09@gmail.com>
2025-07-24 10:51:16 +0500

    test

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,13 +1,13 @@
 import Hero from "../Components/Hero"
+import HomeShop from "../Components/HomeShop"
 import Section from "../Components/Section"
 
 export const Shop = () => {
   return (
     <div className="container mx-auto">
-
+      <HomeShop/>
     <Section/>
     <Hero/>
-     
     </div>
   )
 }
